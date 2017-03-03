62d5a744840541088f6d98d2e6500f60b5781df0
Eric Biel <eric.j.biel@gmail.com>
2017-03-02 19:26:15 -0600

    Modify form order

--- app/controllers/customer_requests_controller.rb
+++ app/controllers/customer_requests_controller.rb
@@ -1,16 +1,22 @@
 class CustomerRequestsController < ApplicationController
 
   def index
+    @requests = current_company.eligible_customer_requests
+    @company = current_company
     @requests = CustomerRequest.all
     @attributes = Company.column_names
     render "index.html.erb"
   end
 
   def new
+    @customer_request = CustomerRequest.new
+    @categories = ServiceCategory.all
     render "new.html.erb"
   end
 
   def create
+    @request = CustomerRequest.new(customer_request_params)
+    @request.customer_id = current_customer.id
     @request = CustomerRequest.new(
       name: params["name"],
       email: params["email"],
@@ -24,10 +30,12 @@ class CustomerRequestsController < ApplicationController
       )
     @request.save
     flash[:success] = "You did it!"
+    redirect_to "/customers/#{current_customer.id}"
     redirect_to '/requests'
   end
 
   def show
+    @request = CustomerRequest.find(params[:id])
     render "show.html.erb"
   end
 
@@ -38,5 +46,19 @@ class CustomerRequestsController < ApplicationController
   end
 
   def destroy
+  end
+
+private
+
+def customer_request_params
+  params.require(:customer_request).permit(
+    :address,
+    :city,
+    :state,
+    :zipcode,
+    :description,
+    :expires_date,
+    :service_category_id
+    )
   end
 end
